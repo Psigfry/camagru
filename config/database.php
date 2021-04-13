540f6cdd0052d069a153f38f57e7c783add588b7
Psigfry <psigfry@protonmail.com>
2021-04-13 10:57:21 +0300

    db

--- config/database.php
+++ config/database.php
@@ -1,5 +1,5 @@
 <?php
 $DB_DSN = "mysql:host=localhost";
-$DB_USER = "u756459948_root";
-$DB_PASSWORD = "abinsur003";
-$DB_NAME = "u756459948_camagru";
+$DB_USER = "";
+$DB_PASSWORD = "";
+$DB_NAME = "";
